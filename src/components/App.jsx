c0ae50b996120090b6bc68b58148955107729334
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2022-12-19 23:09:21 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,28 @@ import React, { Component } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 
 export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
   render() {
     return (
       <div>
-        .......
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <button type="button">Add contact</button>
+
+        <h2>Contacts</h2>
+        <ul></ul>
         <GlobalStyle />
       </div>
     );
