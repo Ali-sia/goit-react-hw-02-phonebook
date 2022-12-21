897255e1805bbbed2244261db001e199566682f7
aliesia-zahorui <aliesia@ukr.net>
2022-12-21 17:34:45 +0200

    створення книги

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,56 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 import { GlobalStyle } from './GlobalStyle';
 
+import ContactList from './ContactList';
+import Filter from './Filter';
+import CreateContact from './CreateContact';
+
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  deleteContact = index => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(cont => cont.id !== index),
+    }));
+  };
+
+  addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
   };
 
   render() {
+    const { contacts, filter } = this.state;
+
     return (
       <div>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <button type="button">Add contact</button>
+        <CreateContact onSubmit={this.addContact} />
 
         <h2>Contacts</h2>
-        <ul></ul>
+        <Filter value={filter} onChangeFilter={this.changeFilter} />
+        <ContactList contacts={contacts} onDeleteContact={this.deleteContact} />
+
         <GlobalStyle />
       </div>
     );
