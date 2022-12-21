430eb2a5e65b36fe3951d1dc5ced07d49bdf3092
aliesia-zahorui <aliesia@ukr.net>
2022-12-21 17:33:42 +0200

    малюємо існуючі контакти

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,20 @@
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <p>
+              {name}, {number}
+            </p>
+            <button type="button" onClick={() => onDeleteContact(id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
