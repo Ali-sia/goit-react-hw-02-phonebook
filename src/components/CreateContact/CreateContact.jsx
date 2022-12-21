27ec892e7f263caa86311259dcf5bb3f00d79de4
aliesia-zahorui <aliesia@ukr.net>
2022-12-21 17:34:25 +0200

    додавання контактів

---
+++ src/components/CreateContact/CreateContact.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class CreateContact extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+  handleSubmit = e => {
+    const { name, number } = this.state;
+    e.preventDefault();
+
+    this.props.onSubmit(name, number);
+    this.setState({
+      name: '',
+      number: '',
+    });
+    e.target.name.value = '';
+    e.target.number.value = '';
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label>
+          Number:
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+// const CreateContact = () => {
+//   return (
+
+//   );
+// };
+
+export default CreateContact;
